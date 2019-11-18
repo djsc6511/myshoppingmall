482153bdb0f9859e36d72147384cf430b0638536
root <djsc6511@naver.com>
2019-11-18 09:01:18 +0000

    home&contact page layout mod & change contact

--- myshoppingmall/page/views.py
+++ myshoppingmall/page/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'page/index.html')
 
-def about(request):
-    return render(request, 'page/about.html')
+def contact(request):
+    return render(request, 'page/contact.html')

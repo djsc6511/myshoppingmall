13ff3d3f142ddae330b9203eda846d225f3f3b19
root <djsc6511@naver.com>
2019-11-18 05:36:17 +0000

    template & base layout

--- myshoppingmall/page/views.py
+++ myshoppingmall/page/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>hello world<h1>")
+    return render(request, 'page/index.html')
+
+def about(request):
+    return render(request, 'page/about.html')
